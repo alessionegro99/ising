88bd716b09e8c654a809944023232551b9acaaa3
alessionegro99 <alessio.negro1999@gmail.com>
2024-12-06 15:48:26 +0100

    Initial commit

---
+++ ising.cpp
@@ -0,0 +1,10 @@
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <random>
+#include <sstream>
+#include <vector>
+
+int main(){
+    std::cout << "Hello world!" << std::endl;
+}
